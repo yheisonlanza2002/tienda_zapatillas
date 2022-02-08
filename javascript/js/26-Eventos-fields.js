9bf42792483ca4e36192345b41e3275ea7cd3d7b
yheison <yheisonlanza@gmail.com>
2022-02-08 17:33:46 -0500

    validando formulario

---
+++ javascript/js/26-Eventos-fields.js
@@ -0,0 +1,7 @@
+function validarInfo(numeroDocumento){
+    let numeroDocu = document.getElementById(numeroDocumento).value;
+
+if(numeroDocu === ""){
+    alert('el numero de documento no puede estar vacio');
+    }
+}
