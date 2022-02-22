1317a17b0d5e90274049477988181b780ee09ae8
yheison lanza <yheisonlanza@gmail.com>
2022-02-22 09:28:14 -0500

    trabajo netflix

---
+++ javascript/js/ejercicio-neflix.js
@@ -0,0 +1,31 @@
+function cambiar(id,ruta) {
+    // alert("olis");
+    document.getElementById(id).src=ruta;
+}
+function retornoImg() {
+    document.getElementById("img").src="img/R (1).jpg";
+}
+
+function cambiar2(id,ruta2) {
+    // alert("olis");
+    document.getElementById(id).src=ruta2;
+}
+function retornoImg2() {
+    document.getElementById("img1").src="img/OIP.jpg";
+}
+
+function cambiar3(id,ruta3) {
+    // alert("olis");
+    document.getElementById(id).src=ruta3;
+}
+function retornoImg3() {
+    document.getElementById("img2").src="img/aven.jpg";
+}
+
+function cambiar4(id,ruta4) {
+    // alert("olis");
+    document.getElementById(id).src=ruta4;
+}
+function retornoImg4() {
+    document.getElementById("img3").src="img/luci.jpg";
+}
